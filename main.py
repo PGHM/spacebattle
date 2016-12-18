090f8d428df932633d2a468bf837dc2c06da2041
ottopoika6 <paivinenotto@gmail.com>
2016-12-18 15:49:51 +0200

    Triangle drawn.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Rect
 
+
 def main():
     """ Set up the game and run the main game loop """
     pygame.init()      # Prepare the pygame module for use
@@ -10,10 +11,13 @@ def main():
     # Create surface of (width, height), and its window.
     main_surface = pygame.display.set_mode((surface_height, surface_width))
 
+    some_color = (0, 0, 255)  # A color is a mix of (Red, Green, Blue)
+    some_other_color = (255, 255, 255)
     # Set up some data to describe a small rectangle and its color
-    small_box = Rect(surface_width / 2, surface_height / 2,50 ,50)
-    some_color = (0, 0, 0)        # A color is a mix of (Red, Green, Blue)
-    some_other_color = (255,255,255)
+    triangle_points = (0,200),(100,0),(200,200)
+
+
+
 
     while True:
         ev = pygame.event.poll()    # Look for any event
@@ -25,8 +29,8 @@ def main():
         # We draw everything from scratch on each frame.
         # So first fill everything with the background color
 
-        main_surface.fill(some_color, small_box)
-        main_surface.fill(some_other_color, small_box)
+        main_surface.fill(some_color)
+        pygame.draw.polygon(main_surface, some_other_color, triangle_points, 0)
 
         # Overpaint a smaller rectangle on the main surface
 
