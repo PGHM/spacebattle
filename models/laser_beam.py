26e3df06819a3ab5ab93c6ebb3691c181aa5067f
Oskari Päivinen <oskari.paivinen@gmail.com>
2016-12-19 00:44:46 +0200

    forgot to add the laser_beam.py file

---
+++ models/laser_beam.py
@@ -0,0 +1,45 @@
+import pygame as pg
+from math import sin
+from math import cos
+from math import pi
+
+
+class LaserBeam:
+
+    def __init__(self, color, starting_pos, direction):
+        self.beam_length = 20
+        self.speed = 10
+
+        self.color = color
+        # self.damage = damage
+        self.direction = direction
+        self.starting_pos = starting_pos
+        self.pos_tail = starting_pos
+        self.pos_front = [starting_pos[0] + self.beam_length * sin(self.direction), starting_pos[1] - self.beam_length * cos(self.direction)]
+       
+        self.main_surface = pg.display.get_surface()
+
+    def draw(self):
+        if self.reached_edge():
+            return False
+        pg.draw.line(self.main_surface, self.color, self.pos_tail, self.pos_front, 3)
+        return True 
+
+    def update_position(self):
+        print (self.direction)
+        tail_x = self.pos_tail[0] + self.speed * sin(self.direction)
+        tail_y = self.pos_tail[1] - self.speed * cos(self.direction)
+
+        front_x = self.pos_tail[0] + self.beam_length * sin(self.direction)
+        front_y = self.pos_tail[1] -self.beam_length * cos(self.direction)
+
+        self.pos_tail = (tail_x, tail_y)
+        self.pos_front = (front_x, front_y )
+
+    def reached_edge(self):
+        x_out_of_bounds = self.pos_front[0] < 0 or self.pos_front[0] > self.main_surface.get_width()
+        y_out_of_bounds = self.pos_front[1] < 0 or self.pos_front[1] > self.main_surface.get_height()
+
+        if x_out_of_bounds or y_out_of_bounds:
+            return True
+        return False
